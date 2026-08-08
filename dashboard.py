c40f82d62cd6f23bd33dd3f6cf96d07c58a32a54
Bogdan Cucu <cucu_bogdan00@yahoo.com>
2026-08-08 20:44:21 +0300

    Feature: Add dynamic search and filtering to jobs dashboard

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import sqlite3
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from config import DB_NAME
+from typing import Optional
 
 app = FastAPI()
 
@@ -10,26 +11,59 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get('/')
 
-def read_jobs(request: Request):
-
-    connection = sqlite3.connect(DB_NAME)
+def read_jobs(
+    request: Request,
+    search: Optional[str] = None,
+    work_mode: Optional[str] = None,
+    experience: Optional[str] = None,
+    source: Optional[str] = None
+):
     
+    connection = sqlite3.connect(DB_NAME)
     connection.row_factory = sqlite3.Row  
     cursor = connection.cursor()
 
-    cursor.execute("""
+    base_query = """
     SELECT id, title, company, location, experience, city, work_mode, link, technologies, date_scraped, source, status
     FROM jobs
     WHERE status = 'active'
-    ORDER BY date_scraped DESC
-    """)
+    """
 
+    params = []
+
+    if search:
+        base_query += " AND (title LIKE ? OR company LIKE ? OR technologies LIKE ? or city LIKE ?)"
+        search_term = f"%{search}%"
+        params.extend([search_term, search_term, search_term, search_term])
+
+    if work_mode:
+        base_query += " AND work_mode = ?"
+        params.append(work_mode)
+
+    if experience:
+        base_query += " AND experience = ?"
+        params.append(experience)
+
+    if source:
+        base_query += " AND source = ?"
+        params.append(source)
+
+    base_query += " ORDER BY date_scraped DESC"
+
+    cursor.execute(base_query, params)
     jobs = cursor.fetchall()
     connection.close()
 
     return templates.TemplateResponse(
         request=request, 
         name="index.html", 
-        context={"request": request, "jobs": jobs}
+        context={
+            "request": request, 
+            "jobs": jobs,
+            "current_search" : search or "",
+            "current_work_mode" : work_mode or "",
+            "current_experience" : experience or "",
+            "current_source" : source or ""
+            }
     )
     
